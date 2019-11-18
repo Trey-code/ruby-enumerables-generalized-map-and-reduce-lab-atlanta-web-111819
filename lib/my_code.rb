d3d3746b6d124ea273dbe434dd55c65eccc9f891
Treven Womack <keytonwomack@yahoo.com>
2019-11-18 11:11:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,3 +1,27 @@
 def map
-  
+  new= []
+  i = 0 
+  while i < array.length 
+  new.push(yield(array[i]))
+  i+= 1 
 end
+new 
+end
+
+def reduce(array, sv=nil)
+  if sv 
+    sum= sv 
+    i=0 
+  else 
+    num1= array[0]
+    i= 1
+  end
+  
+  while i < array.length
+   sum =  yield(num1, array[i])
+   i += 1 
+ end 
+ sum 
+ end 
+  
+  end
